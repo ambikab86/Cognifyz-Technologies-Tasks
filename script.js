aee7198a6ccfc086dcc40396c35d0992a08e7506
ambikab86 <ambikabansode86@gmail.com>
2025-12-16 23:36:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    if (document.body.style.backgroundColor === "lightblue") {
+        document.body.style.backgroundColor = "lightpink";
+    } else {
+        document.body.style.backgroundColor = "lightblue";
+    }
+}
